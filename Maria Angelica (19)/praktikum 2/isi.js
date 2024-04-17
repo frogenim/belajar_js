01960754b20b3eae8a9a5aa39066b1fb012ff748
unknown <mariaangelicafc21@gmail.com>
2024-04-17 14:24:58 +0700

    first commit

---
+++ Maria Angelica (19)/praktikum 2/isi.js
@@ -0,0 +1,9 @@
+confirm("Apakah Anda merupakan Siswa SMKN 4 Malang");
+
+document.writeln("<CENTER>");
+document.writeln("<HR WIDTH-1350 COLOR-Black>");
+document.writeln("<H1>SMK NEGERI 4 MALANG</H1>");
+document.writeln("<H2>PROGRAM KEAHLIAN PENGEMBANGAN PERANGKAT LUNAK DAN GIM</H2>");
+document.writeln("<H3>KELAS X RPL A</H3>");
+document.writeln("<HR WIDTH-1350 COLOR-Black>");
+document.writeln("</CENTER>");
